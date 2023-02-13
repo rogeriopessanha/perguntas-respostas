044e9d129d0923a24b96aadb6983af314d2dfa86
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-02-13 01:42:21 -0300

    att

--- index.js
+++ index.js
@@ -3,8 +3,18 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get('/', (req, res) =>{
-    res.render('index.ejs');
+app.get('/:nome/:lang', (req, res) =>{
+    var nome = req.params.nome;
+    var lang = req.params.lang;
+    var exibirMsg = false;
+
+    res.render('index.ejs',{
+        nome: nome,
+        lang: lang,
+        empresa: 'Guia do programador',
+        inscritos: 1000,
+        msg: exibirMsg
+    });
 });
 
 app.listen(8080, ()=>{console.log('App rodando')
