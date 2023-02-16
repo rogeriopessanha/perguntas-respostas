7ea67647f2f2cf7f208667ed57f4584c5a539914
Rogerio Pessanha <rogerioo_pessanha@yahoo.com.br>
2023-02-16 02:10:49 -0300

    att

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express =  require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
 //estou dizendo para o express usar o ejs como view engine
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
+//body parser
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+
+//rotas
 app.get('/', (req, res) => {
     res.render('index')
 })
@@ -14,7 +21,10 @@ app.get('/perguntar', (req, res) =>{
 })
 
 app.post('/salvarpergunta', (req, res) =>{
+    var titulo = req.body.titulo;
+    var assunto = req.body.assunto;
 
+    res.send('formulario recebido titulo: ' + titulo + ' ' + ' assunto: ' + assunto);
 })
 
 app.listen(8080, () => {
